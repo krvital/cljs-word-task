4f682872e39f47f088ace65c52d7aef277d56df3
Vitaly Kravtsov <mail@krvital.pro>
2022-01-07 23:22:47 +0600

    add an ability to provide multiple correct answers
    refactor template engine as well

--- src/word_task/core.cljs
+++ src/word_task/core.cljs
@@ -1,6 +1,6 @@
 (ns word-task.core
   (:require [rum.core :as rum]
-            [goog.dom.forms :as gforms]))
+            [clojure.string :as str]))
 
 (enable-console-print!)
 
@@ -10,13 +10,14 @@
 
 
 ;; State management
-(def default-text  "What {are} you doing?")
 
-(def state
-  (atom
-   {:task-value default-text
-    :task-parts '()
-    :answers {}}))
+
+(def default-state
+  {:task-value "What {{ are | the hell are }} you doing? Where {{ have }} you been?"
+   :task-parts nil
+   :answers {}})
+
+(def state (atom default-state))
 
 (defn- selector [state path]
   (rum/cursor-in state path))
@@ -25,28 +26,38 @@
 (def task-parts (selector state [:task-parts]))
 (def answers (selector state [:answers]))
 
+(defn- split-into-chunks [text]
+  (str/split text #"(?i)(\{\{.*?\}\})"))
+
+(defn- find-all-objects [text]
+  (set (re-seq #"(?i)\{\{.*?\}\}" text)))
+
+(defn- get-object-value [object]
+  (set (map str/trim (str/split (str/replace object #"[\{\}]" "") "|"))))
+
 (defn- parse-template [text]
-  (def pattern #"(?i)(\{\w+\})")
-
-  (def chunks (clojure.string/split text pattern))
-  (def matches (set (re-seq #"(?i)\{\w+\}" text)))
-
-  (for [chunk chunks]
-    (if (contains? matches chunk)
-      {:type "input" :props {:answer (clojure.string/replace chunk #"[\{\}]" "")
-                             :uuid (gensym "task-")}}
-      {:type "text" :props {:value chunk :uuid (gensym "text-")}})))
+  (let
+   [chunks (split-into-chunks text)
+    objects (find-all-objects text)]
+    (for [chunk chunks]
+      {:uuid (gensym "chunk-")
+       :type (if (contains? objects chunk) "input" "text")
+       :props (if (contains? objects chunk)
+                {:answers (get-object-value chunk)}
+                {:value chunk})})))
 
 (defn save-task [text]
-  (swap! state assoc :task-parts (parse-template text)))
+  (swap! state assoc :task-parts (parse-template text) :task-value text))
 
-(defn save-answer [uuid correct-answer value]
+(defn save-answer [uuid correct-answers value]
   (swap! state assoc-in [:answers uuid]
          {:value value
-          :status (if (= value correct-answer) "correct" "wrong")}))
+          :status (if (contains? correct-answers value) "correct" "wrong")}))
 
 
 ;; Components
+
+
 (rum/defc layout [& children]
   [:div.layout
    (for [child children] [:div.column {:key (gensym)} child])])
@@ -57,38 +68,37 @@
    [:form
     [:textarea {:auto-focus true
                 :name "task-text"
-                :cols 30
+                :cols 40
                 :id "task-text"
                 :default-value task-value}]
     [:br]
-    [:button
-     {:type "button"
-      :on-click #(save-task (input-value "task-text"))}
+    [:button {:type "button"
+              :on-click #(save-task (input-value "task-text"))}
      "Save"]]])
 
-(rum/defc input [answer uuid status]
+(rum/defc input [uuid correct-answers status]
   [:input.task-input
    {:type "text"
-    :size (count answer)
+    :size (apply max (map count correct-answers))
     :class status
-    :on-blur #(save-answer uuid answer (target-value %))}])
+    :on-blur #(save-answer uuid correct-answers (target-value %))}])
 
 (rum/defc task < rum/reactive [parts]
-  (let [answers  (rum/react answers)]
+  (let [answers (rum/react answers)]
     (for [part parts]
-      [:span {:key (:uuid (:props part))}
+      [:span {:key (:uuid part)}
        (case (:type part)
          "input" (input
-                  (:answer (:props part))
-                  (:uuid (:props part))
-                  (:status (get answers (:uuid (:props part)))))
+                  (:uuid part)
+                  (:answers (:props part))
+                  (:status (get answers (:uuid part))))
          "text" (:value (:props part)))])))
 
-(rum/defc answers-list < rum/reactive []
-  (let [ans (vals (rum/react answers))]
-    [:ul
-     (for [a ans]
-       [:li (str (:value a) " - " (:status a))])]))
+;; (rum/defc answers-list < rum/reactive []
+;;   (let [ans (vals (rum/react answers))]
+;;     [:ul
+;;      (for [a ans]
+;;        [:li (str (:value a) " - " (:status a))])]))
 
 (rum/defc preview [parts]
   [:div
