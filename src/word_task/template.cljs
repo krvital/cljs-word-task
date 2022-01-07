87bc2b166cf2abf5a3ffd5cb92c9fc00f3ba808f
Vitaly Kravtsov <mail@krvital.pro>
2022-01-08 05:48:28 +0600

    add docs for template module

--- src/word_task/template.cljs
+++ src/word_task/template.cljs
@@ -1,16 +1,22 @@
 (ns word-task.template
   (:require [clojure.string :as str]))
 
-
+;; split text into chunks
+;; "This is {{ the }} text" -> ["This is " "{{ the }}" "text"]
 (defn- split-into-chunks [text]
   (str/split text #"(?i)(\{\{.*?\}\})"))
 
+;; find all text objects meet the pattern
+;; "This is {{ the }} text" -> #{"{{ the }}"}
 (defn- find-all-objects [text]
   (set (re-seq #"(?i)\{\{.*?\}\}" text)))
 
 (defn- get-object-value [object]
   (set (map str/trim (str/split (str/replace object #"[\{\}]" "") "|"))))
 
+;; functions takes a template string such as "This is {{ the }} template string"
+;; and parses it into collection of items with structure suitable for using
+;; in a react component
 (defn parse-template [text]
   (let
    [chunks (split-into-chunks text)
